b32e30419a0a62529335246fab33d886aec14626
sanketan123 <sanketsomavanshi@gmail.com>
2020-02-27 12:44:25 +0530

    2 files modified, 7 added, 1 deleted

---
+++ src/app/Core/core.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-core',
+  templateUrl:'core.component.html',
+  styleUrls:['core.component.css']
+})
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+//import {CoreComponent} from './core/core.component';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import {LoginComponent} from './login/login.component';
+import {FormGroup} from '@angular/forms';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent,LoginComponent],
+  bootstrap:    [ AppComponent,LoginComponent ]
 })
 export class AppModule { }
