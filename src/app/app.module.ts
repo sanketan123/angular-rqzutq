543f65c1043584544d23935847a10c3860c74d54
sanketan123 <sanketsomavanshi@gmail.com>
2020-03-06 12:40:31 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-//import {CoreComponent} from './core/core.component';
+import {CoreComponent} from './Core/core.component';
 import { AppComponent } from './app.component';
 import {LoginComponent} from './login/login.component';
 import {FormGroup} from '@angular/forms';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent,LoginComponent],
+  declarations: [ AppComponent,LoginComponent,CoreComponent],
   bootstrap:    [ AppComponent,LoginComponent ]
 })
 export class AppModule { }
